974c338408378ae3d6a701c4b36a8ee9c61dd996
Asef Ahmed Shimanto <itsmeshimantokhan@gmail.com>
2025-07-15 00:28:45 +0600

    Add task submission for Asef-Ahmed-Shimanto-24241237

--- sirajam-munir-autonomous-23201065/Task 3a/main.py
+++
@@ -1,5 +0,0 @@
-from ultralytics import YOLO
-
-# Load a model
-model = YOLO("yolov8n.pt")  # build a pretrained model from scratch
-results = model.train(data="data.yaml", epochs=50)  # train the model 
